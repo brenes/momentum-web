a6cdfe039ada9dd3d0e6301b3b7d3c0f8cabd06d
Brenes <shholmes@gmail.com>
2011-01-09 20:44:51 +0100

    Empty rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MomentumWeb::Application.config.secret_token = 'afb8d0279349ba3adb01bd00c8a7e9127099b4bfb58fe853ae0b3b3120f1f9ba2f9b2b2a5a42cdd20a3be5b90e268d677abc248f0ec0037062be2d648b0b77f3'
